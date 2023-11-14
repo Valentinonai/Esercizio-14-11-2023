5b46f4576ae1ce61a0e1342ab496f0a30ce7c7ec
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-14 13:17:00 +0100

    creati endpoint /me

--- src/main/java/Esercizio14112023Progetto/Esercizio14112023Progetto/controllers/UserController.java
+++ src/main/java/Esercizio14112023Progetto/Esercizio14112023Progetto/controllers/UserController.java
@@ -9,6 +9,8 @@ import Esercizio14112023Progetto.Esercizio14112023Progetto.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -25,15 +27,18 @@ public class UserController {
     @Autowired
     private DispositivoService dispositivoService;
     @GetMapping()
+    @PreAuthorize("hasAuthority('ADMIN')")
     public Page<User> getAllUsers(@RequestParam(defaultValue = "0")int page, @RequestParam(defaultValue = "5")int size, @RequestParam(defaultValue = "id") String order){
         return userService.getAllUsers(page,size>20?5:size,order);
     }
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public User getSingleUser(@PathVariable int id){
         return userService.getSingleUser(id);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public User modifyUser(@RequestBody @Validated UserPayload userPayload,BindingResult validation,@PathVariable int id){
         if(validation.hasErrors()){
             throw new BadRequest(validation.getAllErrors());
@@ -41,6 +46,7 @@ public class UserController {
         return userService.modifyUser(userPayload,id);
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable int id)
     {
@@ -49,13 +55,34 @@ public class UserController {
 
 
     @PutMapping("/{id}/image")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public User modifyImage(@PathVariable int id,@RequestParam("immagineProfilo")MultipartFile file) throws IOException {
         return userService.modifyImage(file,id);
     }
 
 
     @GetMapping("/{id}/dispositivi")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<Dispositivo> getDispositiviById(@PathVariable int id){
         return userService.getDispositiviById(id);
     }
+
+
+    //----------ENDPOINT /me---------
+
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@AuthenticationPrincipal User u)
+    {
+        userService.deleteUser(u.getId(),dispositivoService);
+    }
+    @GetMapping("/dispositivi/me")
+    public List<Dispositivo> getDispositiviById(@AuthenticationPrincipal User u){
+        return userService.getDispositiviById(u.getId());
+    }
+    @PutMapping("/image/me")
+    public User modifyImage(@AuthenticationPrincipal User u,@RequestParam("immagineProfilo")MultipartFile file) throws IOException {
+        return userService.modifyImage(file,u.getId());
+    }
+
 }
